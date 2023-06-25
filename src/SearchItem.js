69cbfc8fed1821fa8396012ed967f249a344f4e7
SameerSunkada <sunkadasameer18@gmail.com>
2023-06-25 08:27:31 +0530

    removed role in input element of form in SearchItem

--- src/SearchItem.js
+++ src/SearchItem.js
@@ -7,7 +7,6 @@ const SearchItem = ({search, setSearch}) => {
         <input 
             id='search'
             type="text" 
-            role='searchBox'
             placeholder='Search Items'
             value = {search}
             onChange={(e) => {setSearch(e.target.value)}}
